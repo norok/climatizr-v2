1ef31ef30947b9c99c539fa2ae77fa849563277d
Ivan Lewaschiw <thexsonic@gmail.com>
2017-05-01 14:44:15 -0300

    Making the autocomplete work with the objects.

--- src/app/components/functional/city-selector/city-selector.component.ts
+++ src/app/components/functional/city-selector/city-selector.component.ts
@@ -1,7 +1,10 @@
+import { City } from '../../../classes/city';
 import { NgForm } from '@angular/forms/src/directives';
 import { State } from '../../../classes/state';
 import { CitiesStatesService } from '../../../services/cities-states.service';
 import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
 
 @Component({
   selector: 'cl2-city-selector',
@@ -14,7 +17,7 @@ export class CitySelectorComponent implements OnInit {
   private states:State[];
   private currentState: State;
   private ready:Boolean = false;
-  private autocomplete:any;
+  private formCity:string = '';
 
   constructor(
     private citiesStatesService: CitiesStatesService
@@ -29,17 +32,54 @@ export class CitySelectorComponent implements OnInit {
         .getLocations()
         .then(states => {
           this.states = states;
+          this.currentState = this.states[0];
+          this.updateAutocomplete();
           this.ready = true;
         });
   }
 
   private stateChange(state:State):void {
     this.currentState = state;
+
+    this.updateAutocomplete();
+  }
+
+  private updateAutocomplete():void {
+    let that = this;
+    let citiesList = this.currentState.getCities();
+
+    $('#form-city').autocomplete({
+      source: function(request, response) {
+        var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+        response(
+          citiesList.filter((value:any) => {
+            return matcher.test( value.getName() );
+          })
+        );
+      },
+      minLenght: 0,
+      focus: function(event, ui) {
+        that.formCity = ui.item.getName();
+        return false;
+      },
+      select: function(event, ui) {
+        that.formCity = ui.item.getName();
+        return false;
+      },
+    })
+    .autocomplete('instance')._renderItem = function(ul, item:City) {
+      return $('<li>')
+        .append(item.getName())
+        .appendTo(ul);
+    };
   }
 
   private onSubmit(f: NgForm):void {
-    console.log(f.controls['form-city']);
+    console.log(f.controls['form-city'].value);
     console.log(f.controls['form-state'].value.getAbbr());
+
+    console.log(this.currentState.getName());
+    console.log(this.formCity);
   }
 
 }
