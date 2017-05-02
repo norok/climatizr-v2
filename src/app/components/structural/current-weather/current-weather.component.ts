e37a4d5776ab8cb48a5d53f5a47f6595418c90c9
Ivan Lewaschiw <thexsonic@gmail.com>
2017-05-01 22:21:16 -0300

    Make the CurrentWeatherComponent a subclass of WeatherBlock.

--- src/app/components/structural/current-weather/current-weather.component.ts
+++ src/app/components/structural/current-weather/current-weather.component.ts
@@ -1,72 +1,36 @@
+import { WeatherBlock } from '../../../classes/weather-block';
 import { Subscription } from 'rxjs/Rx';
 import { WeatherService } from '../../../services/weather.service';
 import { GeoLocation } from '../../../classes/geo-location';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
-declare var Skycons:any;
 
 @Component({
   selector: 'cl2-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.scss']
 })
-export class CurrentWeatherComponent implements OnInit {
+export class CurrentWeatherComponent extends WeatherBlock implements OnInit {
 
-  private skycons:any;
   private weatherData:any = {};
   private currentWeather:any = this.weatherData;
   private todayForecast:any = {};
-  private subscription:Subscription;
 
   constructor(private weatherService:WeatherService) {
+    super();
     weatherService.weatherInformation$.subscribe(
-      (data) => {
-        this.currentWeather = data.currently;
-        this.todayForecast = data.daily.data[0];
-
-        this.setIcon('icon-current', this.currentWeather.icon);
-      }
+      (data) => this.setWeatherData(data)
     );
   }
 
   ngOnInit() {
-    this.skycons = new Skycons({"color": "#FFF"});
+    super.ngOnInit();
   }
 
-  private setIcon(id, iconId) {
-    switch (iconId) {
-      case 'clear-day':
-        this.skycons.set(id, Skycons.CLEAR_DAY);
-        break;
-      case 'clear-night':
-        this.skycons.set(id, Skycons.CLEAR_NIGHT);
-        break;
-      case 'partly-cloudy-day':
-        this.skycons.set(id, Skycons.PARTLY_CLOUDY_DAY);
-        break;
-      case 'partly-cloudy-night':
-        this.skycons.set(id, Skycons.PARTLY_CLOUDY_NIGHT);
-        break;
-      case 'cloudy':
-        this.skycons.set(id, Skycons.CLOUDY);
-        break;
-      case 'rain':
-        this.skycons.set(id, Skycons.RAIN);
-        break;
-      case 'sleet':
-        this.skycons.set(id, Skycons.SLEET);
-        break;
-      case 'snow':
-        this.skycons.set(id, Skycons.SNOW);
-        break;
-      case 'wind':
-        this.skycons.set(id, Skycons.WIND);
-        break;
-      case 'fog':
-        this.skycons.set(id, Skycons.FOG);
-        break;
-    }
-    this.skycons.play();
+  private setWeatherData(data):void {
+    this.currentWeather = data.currently;
+    this.todayForecast = data.daily.data[0];
+
+    this.setIcon('icon-current', this.currentWeather.icon);
   }
 
 }
