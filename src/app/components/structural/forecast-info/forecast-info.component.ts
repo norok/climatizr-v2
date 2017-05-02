d741d947871eff28d93c230b699c368d89de3517
Ivan Lewaschiw <thexsonic@gmail.com>
2017-05-01 22:36:49 -0300

    Add the owl-carousel to the weekly forecast items.

--- src/app/components/structural/forecast-info/forecast-info.component.ts
+++ src/app/components/structural/forecast-info/forecast-info.component.ts
@@ -12,21 +12,53 @@ export class ForecastInfoComponent implements OnInit {
 
   private forecast:Array<any> = [];
   private todos:Array<string> = [];
+  private forecastCarousel:any;
 
   constructor(private weatherService:WeatherService) {
     weatherService.weatherInformation$.subscribe(
-      (data) => {
-        let array:Array<any> = data.daily.data;
-        array.shift(); // Removes the current day since we don't need it
-        this.forecast = array;
-
-        setTimeout(() => {
-          $('[data-toggle="popover"]').popover();
-        })
-      }
+      (data) => this.updateData(data)
     );
   }
 
   ngOnInit() {}
+
+  private updateData(data) {
+    let array:Array<any> = data.daily.data;
+    array.shift(); // Removes the current day since we don't need it
+    this.forecast = array;
+
+    setTimeout(() => {
+      $('[data-toggle="popover"]').popover();
+    })
+
+    this.updateView();
+  }
+
+  private updateView():void {
+    if (!!this.forecastCarousel) {
+      // Destroy the owl carousel to build a new one with new data
+      this.forecastCarousel.trigger('destroy.owl.carousel').removeClass('owl-drag owl-loaded');
+      this.forecastCarousel.find('.owl-stage-outer').children().unwrap();
+    }
+
+    setTimeout(() => {
+      this.forecastCarousel = $('#forecast-carousel').owlCarousel({
+        responsive: {
+          0: {
+            items: 2,
+          },
+          600: {
+            items: 4,
+          },
+          995: {
+            items: 6,
+          },
+          1200: {
+            items: 7,
+          }
+        }
+      });
+    });
+  }
 
 }
