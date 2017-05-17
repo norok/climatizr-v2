dec8dffd9bc1241a0ba9c44fb1514bab7aa2137b
Ivan Lewaschiw <thexsonic@gmail.com>
2017-05-16 22:28:20 -0300

    Updates the app with the menu and communication between services and components for favorites and navigation.

--- src/app/components/functional/city-selector/city-selector.component.ts
+++ src/app/components/functional/city-selector/city-selector.component.ts
@@ -1,15 +1,17 @@
+import { FavoritesService } from '../../../services/favorites.service';
+import { NavService } from '../../../services/nav.service';
 import { WeatherService } from '../../../services/weather.service';
 import { CurrentWeatherComponent } from '../../structural/current-weather/current-weather.component';
 import { EventEmitter } from 'events';
 import { GeoLocation } from '../../../classes/geo-location';
 import { NgForm } from '@angular/forms/src/directives';
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output } from '@angular/core';
 
 import { City } from '../../../classes/city';
 import { State } from '../../../classes/state';
 import { CitiesStatesService } from '../../../services/cities-states.service';
 import { LocationService } from '../../../services/location.service';
-import { LocalstorageService } from '../../../services/localstorage.service';
+import { Subscription } from "rxjs";
 
 declare var $:any;
 
@@ -17,55 +19,80 @@ declare var $:any;
   selector: 'cl2-city-selector',
   templateUrl: './city-selector.component.html',
   styleUrls: ['./city-selector.component.scss'],
-  providers: [CitiesStatesService, LocationService, LocalstorageService],
+  providers: [],
 })
-export class CitySelectorComponent implements OnInit {
+export class CitySelectorComponent implements OnInit, OnDestroy {
 
-  public currentState: State;
-  public currentCity:string = '';
-  public ready:Boolean = false;
-  public states:State[];
+  private currentState: State;
+  private currentCity:string = '';
+  private ready:Boolean = false;
+  private states:State[];
 
   private defaults:any = {
     state: 'SP',
     city: 'Campinas'
   }
 
-  private favoriteCity:any = null;
+  private favorites:Array<any> = [];
   private isFavorite:boolean = false;
+  private favSubscription:Subscription;
+  private navSubscription:Subscription;
 
   constructor(
     private citiesStatesService: CitiesStatesService,
     private locationService: LocationService,
     private weatherService:WeatherService,
-    private localStorage:LocalstorageService,
+    private navService:NavService,
+    private favoritesService:FavoritesService,
   ) {}
 
   ngOnInit():void {
+    this.favSubscription = this.favoritesService.favoritedItem$
+      .subscribe(item => {
+        this.getFavorites();
+      });
+
+    this.navSubscription = this.navService.navItem$
+      .subscribe(nav => {
+        this.stateChange(this.getStateByAbbr(nav.state));
+        this.currentCity = nav.city;
+        this.updateLocation();
+      });
+
     this.getStates();
+  }
+
+  ngOnDestroy():void {
+    this.navSubscription.unsubscribe();
+    this.favSubscription.unsubscribe();
   }
 
   private getStates():void {
     this.citiesStatesService
-        .getLocations()
-        .then(states => {
-          this.states = states;
+      .getLocations()
+      .then(states => {
+        this.states = states;
+        this.getFavorites();
 
-          this.getFavorite();
+        let fave = !!this.favorites ? this.favorites[0] : false;
+        let favState;
 
-          this.currentCity  = this.favoriteCity == null ? this.defaults.city : this.favoriteCity.city;
-          let favState      = this.favoriteCity == null ? this.defaults.state : this.favoriteCity.state;
+        if (fave) {
+          this.currentCity  = fave.city;
+          favState = fave.state;
+        }
+        else {
+          this.currentCity = this.defaults.city;
+          favState = this.defaults.state;
+        }
 
-          var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( favState ), "i" );
-          this.currentState = this.states.filter((value:any) => {
-            return matcher.test( value.getAbbr() );
-          })[0];
+        this.currentState = this.getStateByAbbr(favState);
 
-          this.updateAutocomplete();
+        this.updateAutocomplete();
 
-          this.updateLocation(this.currentState, this.currentCity);
-          this.ready = true;
-        });
+        this.updateLocation();
+        this.ready = true;
+      });
   }
 
   public stateChange(state:State):void {
@@ -108,13 +135,15 @@ export class CitySelectorComponent implements OnInit {
     event.preventDefault();
 
     if (f.valid) {
-      this.updateLocation(this.currentState, this.currentCity);
+      this.updateLocation();
     }
   }
 
-  private updateLocation(state, city):void {
-    this.getFavorite();
-    this.isFavorite = (state.abbr == this.favoriteCity.state && city == this.favoriteCity.city);
+  private updateLocation():void {
+    let city = this.currentCity;
+    let state = this.currentState;
+
+    this.getFavorites();
 
     this.locationService
         .getPreciseLocation(state, city)
@@ -123,19 +152,34 @@ export class CitySelectorComponent implements OnInit {
         });
   }
 
-  private saveFavorite():void {
-    this.isFavorite = true;
-
-    let object = {
-      'state': this.currentState.getAbbr(),
-      'city': this.currentCity
-    };
-
-    this.localStorage.setJSONData('favoriteCity', object);
+  private getStateByAbbr(abbr:string):State {
+    var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( abbr ), "i" );
+    return this.states.filter((value:any) => {
+      return matcher.test( value.getAbbr() );
+    })[0];
   }
 
-  private getFavorite():void {
-    this.favoriteCity = this.localStorage.getJSONData('favoriteCity');
+  private saveFavorite():void {
+    if (this.currentCityIsFavorite()) {
+      this.favoritesService.removeCity(this.currentCity, this.currentState.getAbbr());
+    }
+    else {
+      this.favoritesService.addCity(this.currentCity, this.currentState.getAbbr());
+    }
+  }
+
+  private getFavorites():void {
+    this.favorites = this.favoritesService.getFavorites();
+  }
+
+  private currentCityIsFavorite():boolean {
+    for (let favorite of this.favorites) {
+      if (favorite.city == this.currentCity && favorite.state == this.currentState.getAbbr()) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
 }
