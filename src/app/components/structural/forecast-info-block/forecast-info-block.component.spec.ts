4d6379f6a017ba89e916e4f859df05af184b8943
norok <ivan.lewas@gmail.com>
2021-02-25 23:15:15 -0300

    Auto update from angular 10 to 11

--- src/app/components/structural/forecast-info-block/forecast-info-block.component.spec.ts
+++ src/app/components/structural/forecast-info-block/forecast-info-block.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ForecastInfoBlockComponent } from './forecast-info-block.component';
 
@@ -6,7 +6,7 @@ describe('ForecastInfoBlockComponent', () => {
   let component: ForecastInfoBlockComponent;
   let fixture: ComponentFixture<ForecastInfoBlockComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ForecastInfoBlockComponent ]
     })
